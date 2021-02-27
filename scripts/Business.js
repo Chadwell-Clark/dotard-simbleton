40238ebc9f53bff94cd9d89a466d05d7211987c7
Chadwell-Clark <wcclark23@comcast.net>
2021-02-27 10:37:37 -0600

    finished forEach() and filter()

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,9 +1,9 @@
 export const Company = (companyObject) => {
     return (`
         <div class="company">
-            <h2 class="companyName">${companyObject.companyName}</h2>
-            <div class="address">${companyObject.addressFullStreet}</div>
-            <div class="">${companyObject.addressCity}, ${companyObject.addressStateCode} ${companyObject.addressZipCode} </div>
+            <h3 class="companyName">${companyObject.companyName}</h3>
+            <div>${companyObject.addressFullStreet}</div>
+            <div>${companyObject.addressCity}, ${companyObject.addressStateCode} ${companyObject.addressZipCode} </div>
         </div>
     `)
 }
