68d48538344ce965f0b2fccfb7ec64a04efa25a4
Chadwell-Clark <wcclark23@comcast.net>
2021-03-10 09:13:07 -0600

    fixed find function

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,11 @@ console.log("main is loaded")
 import { BusinessList1, finder} from "./modules/BusinessList.js";
 
 
-const eventElement = document.querySelector("main")
-eventElement.addEventListener("keydown", event => { 
-        console.log(event) 
-}
-)
+// const eventElement = document.querySelector("main")
+// eventElement.addEventListener("keydown", event => { 
+//         console.log(event) 
+// }
+// )
 
 BusinessList1();
 finder();
--- scripts/modules/BusinessList.js
+++ scripts/modules/BusinessList.js
@@ -1,30 +1,34 @@
-import { getBusinesses, getNewYorkBusinesses, getManufacturingBusinesses, getPurchasingAgents } from "./BusinessData.js";
+import {
+  getBusinesses,
+  getNewYorkBusinesses,
+  getManufacturingBusinesses,
+  getPurchasingAgents,
+} from "./BusinessData.js";
 import { Company } from "./Business.js";
 import { PurchasingAgent } from "./PurchasingAgent.js";
 
-
 export const BusinessList1 = () => {
-    const companyArray = getBusinesses();
-    const DOMTarget = document.querySelector("#companies");
-    companyArray.forEach((company) => {
-        DOMTarget.innerHTML += Company(company)
-    });
+  const companyArray = getBusinesses();
+  const DOMTarget = document.querySelector("#companies");
+  companyArray.forEach((company) => {
+    DOMTarget.innerHTML += Company(company);
+  });
 };
 
 export const BusinessList2 = () => {
-    const companyArray = getNewYorkBusinesses();    
-    const DOMTarget = document.querySelector(".businessList--newYork");
-    companyArray.forEach((company) => {
-        DOMTarget.innerHTML += Company(company)
-    });
+  const companyArray = getNewYorkBusinesses();
+  const DOMTarget = document.querySelector(".businessList--newYork");
+  companyArray.forEach((company) => {
+    DOMTarget.innerHTML += Company(company);
+  });
 };
 
 export const BusinessList3 = () => {
-    const companyArray = getManufacturingBusinesses();    
-    const DOMTarget = document.querySelector(".businessList--manufacturing");
-    companyArray.forEach((company) => {
-        DOMTarget.innerHTML += Company(company)
-    });
+  const companyArray = getManufacturingBusinesses();
+  const DOMTarget = document.querySelector(".businessList--manufacturing");
+  companyArray.forEach((company) => {
+    DOMTarget.innerHTML += Company(company);
+  });
 };
 // export const PurchasingAgents = () => {
 //     const agentArray = getPurchasingAgents();
@@ -37,73 +41,33 @@ export const BusinessList3 = () => {
 // };
 
 export const PurchasingAgentList = () => {
-    const companyArray = getPurchasingAgents();
-    const DOMTarget = document.querySelector(".agents");
-    companyArray.forEach((company) => {
-        DOMTarget.innerHTML += PurchasingAgent(company)
-    });
+  const companyArray = getPurchasingAgents();
+  const DOMTarget = document.querySelector(".agents");
+  companyArray.forEach((company) => {
+    DOMTarget.innerHTML += PurchasingAgent(company);
+  });
 };
-
-// export const finder = () => {
-// const companySearchResultArticle = document.querySelector(".foundCompanies");
-// document
-//   .querySelector("#companySearch")
-//   .addEventListener("keydown", (keyDownEvent) => {
-//     if (keyDownEvent.charCode === 13) {
-//       /*
-//                 When user presses enter, find the matching business.
-//                 You can use the `.includes()` method strings to
-//                 see if a smaller string is part of a larger string.
-
-//                 Example:
-//                     business.companyName.includes(keyPressEvent.target.value)
-//             */
-
-//       const foundBusiness = (companySearchResultArticle.innerHTML =  getBusinesses().find((item)=> {
-//             return item.companyName === BusinessList1.companyName.includes(keyDownEvent.target.value)
-//       })// implement .find() method here
-//       `
-//                 <h2>
-//                 ${foundBusiness.companyName}
-//                 </h2>
-//                 <section>
-//                 ${foundBusiness.addressFullStreet}
-
-//                 </section>
-//                 <section>
-//                 ${foundBusiness.addressCity},
-//                 ${foundBusiness.addressStateCode}
-//                 ${foundBusiness.addressZipCode}
-//                 </section>
-//             `);
-//     }
-//   });
-// }
 
 //!!!!!!      !!!** From chapter 13     ******      !!!!!//
 // Place an article element in your HTML with the class below
 export const finder = () => {
-const companySearchResultArticle = document.querySelector(".foundCompanies")
+  const companySearchResultArticle = document.querySelector(".foundCompanies");
 
-document
+  document
     .querySelector("#companySearch")
-    .addEventListener("keydown", event => {
-        if (event.key === 13) {
-            /*
-                When user presses enter, find the matching business.
-                You can use the `.includes()` method strings to
-                see if a smaller string is part of a larger string.
+    .addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        const foundBusiness = getBusinesses().find((business) =>
+          business.companyName
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase())
+        );
 
-                Example:
-                    business.companyName.includes(keyPressEvent.target.value)
-            */
-
-            const foundBusiness = getBusinesses().find(business => business.companyName.startsWith(event.target.value))
-
-            companySearchResultArticle.innerHTML = `
-                <h2>
+        companySearchResultArticle.innerHTML = `
+            <div class="company">
+                <h3 class="companyName">
                 ${foundBusiness.companyName}
-                </h2>
+                </h3>
                 <section>
                 ${foundBusiness.addressFullStreet}
 
@@ -113,7 +77,8 @@ document
                 ${foundBusiness.addressStateCode}
                 ${foundBusiness.addressZipCode}
                 </section>
+                </div>
             `;
-        }
+      }
     });
-}
+};
